0aa22e7b6ff706e27f56c45e04ec779514c45015
naitik <naitikkherala47@gmail.com>
2024-08-03 17:51:42 +0530

    add database table

--- database_project/App/models.py
+++ database_project/App/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.TextField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
+    password = models.TextField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- database_project/App/views.py
+++ database_project/App/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from App.models import User
 # Create your views here.
+
+def home(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = User(name=name,email=email,password=password)
+        user.save()
+    return render(request, 'home.html')
