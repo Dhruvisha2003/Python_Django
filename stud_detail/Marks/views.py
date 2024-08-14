1ccb7acb719856d71d8ebc0ef8ac3884194f3eca
naitik <naitikkherala47@gmail.com>
2024-08-14 08:53:44 +0530

    updated

--- stud_detail/Marks/views.py
+++ stud_detail/Marks/views.py
@@ -2,11 +2,10 @@ from django.shortcuts import *
 from Marks.models import stud
 
 # Create your views here.
-#create student form that have their name and marks of 5 subject then print total per minimum marks and grade
+
 
 def input(request):
     if request.method == "POST" :
-        try:
             name = request.POST['name']
             marks1 = request.POST['marks1']
             marks2 = request.POST['marks2']
@@ -16,7 +15,7 @@ def input(request):
             total = int(marks1) + int(marks2) +int (marks3) + int(marks4) + int(marks5)
             percentage = (total/500)*100
             if percentage >= 90 :
-                grade = 'A'
+                grade = 'A' 
             elif percentage >= 80 :
                 grade = 'B'
             elif percentage >= 70 :
@@ -31,8 +30,7 @@ def input(request):
             data = stud(name=name,marks1=marks1,marks2=marks2,marks3=marks3,marks4=marks4,marks5=marks5,total=total,percentage=percentage,     grade=grade,min_marks=min_marks)
             data.save()
             return redirect('data_table')
-        except ValueError:
-            return render(request, 'home.html', {'error': 'Invalid input. Please enter valid numbers.'})
+       
     return render(request,'home.html')                
 
 
@@ -55,7 +53,7 @@ def update(request,id):
         marks4 = request.POST['marks4']
         marks5 = request.POST['marks5']
         total = int(marks1) + int(marks2) +int (marks3) + int(marks4) + int(marks5)
-        percentage = float(total/500)*100
+        percentage = (total/500)*100
         if percentage >= 90 :
             grade = 'A'
         elif percentage >= 80 :
