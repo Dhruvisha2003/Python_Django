01fa5589603f9a42b1db7b44adebd58ca84d7178
naitik <naitikkherala47@gmail.com>
2024-08-21 09:21:38 +0530

    updated

--- website_project2/App/urls.py
+++ website_project2/App/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from App import views
 
 urlpatterns = [
-    path('',include('App.urls')),
     path('admin/', admin.site.urls),
-    
+    path('', views.index, name='index'),
 ]
