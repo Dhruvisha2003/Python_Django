76ab3b3de318f45125f18680b23963ed76b2785e
Dhruvisha Vekariya <gdvekariya8265@gmail.com>
2024-08-22 22:39:37 +0530

    updated

--- Furniture_project/App/models.py
+++ Furniture_project/App/models.py
@@ -8,3 +8,7 @@ class products(models.Model):
     name = models.CharField(max_length=20)
     price = models.CharField(max_length=10)
     
+class pdetails(models.Model):
+    photo = models.ImageField()
+    name = models.CharField(max_length=20)
+    detail = models.CharField(max_length=220)
--- Furniture_project/App/views.py
+++ Furniture_project/App/views.py
@@ -3,7 +3,7 @@ from .models import menu
 from .models import products
 
 def index(request):
-    all=menu.objects.all()
-    product = products.objects.all()
-    return render(request, 'index.html',{'all':all,'product':product})
+    menus = menu.objects.all()
+    products_list = products.objects.all()
+    return render(request, 'index.html',{'menus':menus,'products_list':products_list})
 
