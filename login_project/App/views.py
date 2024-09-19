369855f40a273558e6bb1c6ee3b5010a3cb814b3
dhruvkumar <druvzanzmera@gmail.com>
2024-09-19 09:13:55 +0530

    updated

--- login_project/App/views.py
+++ login_project/App/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import *
 from App.models import User
-# Create your views here.
+from django.views.decorators.cache import cache_control
+from django.contrib import messages
 
+# Create your views here.
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def signup(request):
     if request.method == 'POST':
         name = request.POST['name']
@@ -14,14 +17,38 @@ def signup(request):
         return redirect('login')
     return render(request,'register.html')
         
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def signin(request):
     if request.method == 'POST':
+        id=request.POST.get('id')
         email = request.POST.get('email')
         password = request.POST.get('password')
-        data = User.objects.filter(email=email,password=password)
-        if data:
-            return render(request,'home.html')
-        else:
-            return HttpResponse("Invalid Email or Password")
+        try:
+            data = User.objects.get(email=email,password=password)
+            print("Session_id =",data.id)
+            request.session['id'] = data.id
+            print('session set')
+            return redirect('home') 
+        except:
+            messages.error(request, 'User not found in the Register table!!! Please Register ')
+            return redirect('login')
     else:
         return render(request,'login.html')
+    
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)   
+def logout(request):
+    if 'id' in request.session:
+        del request.session['id']
+        print('Deleted session ID')
+      
+    
+    return redirect("/")
+
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+def home(request):
+    data1 = request.session.get('id')
+    if data1:
+        print("=-------------------==-=-=-=-=-=-=-")
+        return render(request,'home.html',{'data1':data1})
+    else:
+        return redirect('login')
--- login_project/login_project/urls.py
+++ login_project/login_project/urls.py
@@ -21,6 +21,7 @@ from App import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/',views.signup,name='register'),
-    path('login/',views.signin,name='login'),
-    path('home/',views.signin)
+    path('',views.signin,name='login'),
+    path('logout',views.logout,name='logout'),
+    path('home/',views.home,name='home')
 ]
